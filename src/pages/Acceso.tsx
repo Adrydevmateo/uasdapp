585c7c27e5f9793c74e56f3fe539e78758587e02
AdryDev <adrydevmateo@gmail.com>
2024-12-01 16:53:05 -0400

    Acceso page finished

--- src/pages/Acceso.tsx
+++ src/pages/Acceso.tsx
@@ -1,6 +1,22 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonButtons, IonContent, IonHeader, IonInput, IonItem, IonList, IonMenuButton, IonPage, IonTitle, IonToolbar, useIonRouter } from '@ionic/react';
+import { useState } from 'react';
+import { Routes } from '../router/types';
 
 const Acceso: React.FC = () => {
+  const router = useIonRouter();
+  const navegar = () => router.push(Routes.MENU)
+
+  const [usuario, setUsuario] = useState("")
+  const [contrasena, setContrasena] = useState("")
+
+  const acceder = () => {
+    const nuevoUsuario = {
+      usuario,
+      contrasena
+    }
+
+    navegar()
+  }
 
   return (
     <IonPage>
@@ -20,7 +36,20 @@ const Acceso: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <div className='ion-padding'>
-          <h1>Acceso</h1>
+          <IonList>
+            <IonItem>
+              <IonInput label="Usuario" labelPlacement="stacked" placeholder="Ingresa tu nombre de usuario" value={usuario} onInput={({ target }) => setUsuario(target.value)}></IonInput>
+            </IonItem>
+            <IonItem>
+              <IonInput label="Contraseña" labelPlacement="stacked" placeholder="Ingresa tu contraseña" type='password' value={contrasena} onInput={({ target }) => setContrasena(target.value)}></IonInput>
+            </IonItem>
+            <br />
+            <div style={{display: "flex", flexWrap: "wrap", justifyContent: "space-between"}}>
+            <IonButton routerLink={Routes.RECUPERACION} fill='clear'>Recuperar Contraseña</IonButton>
+            <IonButton routerLink={Routes.INSCRIPCION} fill='clear'>Estudia con Nosotros</IonButton>
+            </div>
+          </IonList>
+          <IonButton onClick={acceder} expand='full'>Acceder</IonButton>
         </div>
       </IonContent>
     </IonPage>
