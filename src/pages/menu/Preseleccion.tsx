b83eda3d7298b92e3fedcc9bb408908b564b018b
AdryDev <adrydevmateo@gmail.com>
2024-12-01 19:33:10 -0400

    Preseleccion page finished

--- src/pages/menu/Preseleccion.tsx
+++ src/pages/menu/Preseleccion.tsx
@@ -1,6 +1,59 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCheckbox, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { useState } from 'react';
 
 const Preseleccion: React.FC = () => {
+
+  const [materias, setMaterias] = useState([
+    {
+      id: crypto.randomUUID(),
+      nombre: "Nombre",
+      horario: "horario",
+      aula: "Aula 1",
+      seleccionada: false,
+    },
+    {
+      id: crypto.randomUUID(),
+      nombre: "Nombre",
+      horario: "horario",
+      aula: "Aula 2",
+      seleccionada: false,
+    },
+    {
+      id: crypto.randomUUID(),
+      nombre: "Nombre",
+      horario: "horario",
+      aula: "Aula 3",
+      seleccionada: false,
+    },
+    {
+      id: crypto.randomUUID(),
+      nombre: "Nombre",
+      horario: "horario",
+      aula: "Aula 4",
+      seleccionada: false,
+    },
+    {
+      id: crypto.randomUUID(),
+      nombre: "Nombre",
+      horario: "horario",
+      aula: "Aula 5",
+      seleccionada: false,
+    }
+  ])
+
+  const seleccionarMateria = (id: string) => {
+    const listaMateriasActualizadas = materias.map((materia) => {
+      if (materia.id == id) {
+        materia.seleccionada = !materia.seleccionada
+      } 
+      return materia
+    }) 
+    setMaterias(listaMateriasActualizadas)
+  }
+
+  const preseleccionar = () => {
+    
+  }
 
   return (
     <IonPage>
@@ -20,7 +73,16 @@ const Preseleccion: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <div className='ion-padding'>
-          <h1>Preselección</h1>
+          {materias.map((materia, index) => (
+            <IonCard className='ion-padding' key={index} onClick={() => seleccionarMateria(materia.id)}>
+              <IonCheckbox justify="space-between" checked={materia.seleccionada}>
+                <IonCardSubtitle style={{fontSize: "2.1em"}}>{materia.horario}</IonCardSubtitle>
+                <IonCardTitle style={{fontSize: "2.4em"}}>{materia.nombre}</IonCardTitle>
+                <IonCardSubtitle style={{fontSize: "2.2em"}}>{materia.aula}</IonCardSubtitle>
+              </IonCheckbox>
+            </IonCard>
+          ))}
+          <IonButton onClick={preseleccionar} expand='full'>Preseleccionar</IonButton>
         </div>
       </IonContent>
     </IonPage>
