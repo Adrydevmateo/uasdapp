e6f20ebf907b5a76640a9227ee1fe8ee65cd4cd2
AdryDev <adrydevmateo@gmail.com>
2024-12-01 19:16:13 -0400

    Materias page finished

--- src/pages/menu/Materias.tsx
+++ src/pages/menu/Materias.tsx
@@ -1,6 +1,37 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { useState } from 'react';
 
 const Materias: React.FC = () => {
+
+  const [activo, setActivo] = useState("")
+
+  const materias = [
+    {
+      nombre: "Nombre",
+      horario: "horario",
+      aula: "Aula 1",
+    },
+    {
+      nombre: "Nombre",
+      horario: "horario",
+      aula: "Aula 2",
+    },
+    {
+      nombre: "Nombre",
+      horario: "horario",
+      aula: "Aula 3",
+    },
+    {
+      nombre: "Nombre",
+      horario: "horario",
+      aula: "Aula 4",
+    },
+    {
+      nombre: "Nombre",
+      horario: "horario",
+      aula: "Aula 5",
+    }
+  ]
 
   return (
     <IonPage>
@@ -20,7 +51,22 @@ const Materias: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <div className='ion-padding'>
-          <h1>Materias</h1>
+          {materias.map((materia, index) => (
+            <IonCard key={index} onClick={() => setActivo(`${materia.nombre}-${materia.horario}-${materia.aula}`)}>
+              <IonCardHeader>
+                <IonCardSubtitle>{materia.horario}</IonCardSubtitle>
+                <IonCardTitle>{materia.nombre}</IonCardTitle>
+                <IonCardSubtitle>{materia.aula}</IonCardSubtitle>
+              </IonCardHeader>
+              {
+                activo == `${materia.nombre}-${materia.horario}-${materia.aula}` ? (
+                  <IonCardContent>
+                    <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+                  </IonCardContent>
+                ) : null
+              }
+            </IonCard>
+          ))}
         </div>
       </IonContent>
     </IonPage>
